14744085e31f9fe884f46719e54cb255a8e6baa8
MylesTrump <>
2021-05-07 13:22:13 +0000

    Fixed the program

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Myles Trump
+# Created on: May 2021
+# This program figures out the perimeter and area
+
+
+def main():
+    # This function figures out the perimeter and area
+    perimeter = 2 * (5 + 3)
+    area = 5 * 3
+    
+    print("The perimeter is {0} mm and the area is {1} mm².".format(perimeter, area))
+
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
